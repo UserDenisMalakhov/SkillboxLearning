5d271bfd853d76b79b770c5682d0278784f30831
Denis <89623822002@mail.ru>
2023-01-16 12:45:00 +0300

    ex

--- 10 ArrayList/src/main/java/practice/Main1.java
+++ 10 ArrayList/src/main/java/practice/Main1.java
@@ -1,24 +1,24 @@
 package practice;
 
-public class Main1 {
-    // используя данный метод, вы можете использовать ваши методы, и проверить их работу на разных
-    // значений для проверки и отладки//
-    public static void main(String[] args) {
-        String line = "Каждый охотник желает знать, где сидит фазан";
-        String[] colors = line.split(",?\\s++");
-        for (int i = 0; i < colors.length; i++){
-            System.out.println(colors[i]);
-        }
-        for (int j = 0; j < colors.length / 2; j++){
-            String temp = colors[j];
-            colors[j] = colors[colors.length - j -1];
-            colors[colors.length - j -1] = temp;
-        }
-
-        System.out.println("\n");
-
-        for (String color : colors){
-            System.out.println(color);
-        }
-    }
-}
+//public class Main1 {
+//    // используя данный метод, вы можете использовать ваши методы, и проверить их работу на разных
+//    // значений для проверки и отладки//
+//    public static void main(String[] args) {
+//        String line = "Каждый охотник желает знать, где сидит фазан";
+//        String[] colors = line.split(",?\\s++");
+//        for (int i = 0; i < colors.length; i++){
+//            System.out.println(colors[i]);
+//        }
+//        for (int j = 0; j < colors.length / 2; j++){
+//            String temp = colors[j];
+//            colors[j] = colors[colors.length - j -1];
+//            colors[colors.length - j -1] = temp;
+//        }
+//
+//        System.out.println("\n");
+//
+//        for (String color : colors){
+//            System.out.println(color);
+//        }
+//    }
+//}
